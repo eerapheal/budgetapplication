4ce86b2d6f731d6778dff577cf1c36d4a23a5e2e
Erue <ekpenisiraphael@gmail.com>
2023-06-26 15:11:41 +0530

    Validation and complete relationships.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_many :categories, foreign_key: :author_id, dependent: :destroy
+    has_many :expeneses, foreign_key: :author_id, dependent: :destroy
+  
+    validates :name, presence: true
+  
 end
