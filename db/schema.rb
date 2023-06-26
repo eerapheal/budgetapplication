4081d566fc04448ddb6711dd913cc9531c8a394a
Erue <ekpenisiraphael@gmail.com>
2023-06-26 15:13:26 +0530

    Data migrated to schema version.

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_26_093307) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "categories", force: :cascade do |t|
+    t.bigint "author_id", null: false
+    t.string "name"
+    t.string "icon"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_categories_on_author_id"
+  end
+
+  create_table "categoryexpenses", force: :cascade do |t|
+    t.bigint "expense_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_categoryexpenses_on_category_id"
+    t.index ["expense_id"], name: "index_categoryexpenses_on_expense_id"
+  end
+
+  create_table "expenses", force: :cascade do |t|
+    t.string "name"
+    t.decimal "amount"
+    t.bigint "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_expenses_on_author_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "categories", "users", column: "author_id"
+  add_foreign_key "categoryexpenses", "categories"
+  add_foreign_key "categoryexpenses", "expenses"
+  add_foreign_key "expenses", "users", column: "author_id"
 end
