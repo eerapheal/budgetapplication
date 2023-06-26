56a898570f48c7679a7773ffc457f5b1a6075782
Erue <ekpenisiraphael@gmail.com>
2023-06-26 18:59:26 +0530

    Signed up from db.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  root  to: 'user#index'
+ 
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root  to: 'user#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create(name: "Luke Rechard")
+User.create(name: "Luke Rechard", email: "eeer@gmail.com", password: "mmmmmm")
